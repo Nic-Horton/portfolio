37749e234460b6bdc27a1b78383ee52154a965d3
Nic-Horton <nic.horton00@gmail.com>
2023-09-21 16:14:33 -0500

    Added js file for email copied button

---
+++ src/scripts/toCopyEmail.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var copyText = document.getElementById("myEmail");
+  copyText.select();
+  document.execCommand("copy");
+  showSnackBar();
+}
+
+function showSnackBar() {
+  var sb = document.getElementById("snackbar");
+  //this is where the class name will be added & removed to activate the css
+  sb.className = "show";
+  setTimeout(() => {
+    sb.className = sb.className.replace("show", "");
+  }, 2000);
+}
